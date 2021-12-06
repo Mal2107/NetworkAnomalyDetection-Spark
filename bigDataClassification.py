b901cbc43fd0081c7e14b54dc1ea33a45888083f
MAL2107 <pattekar.malhar01@gmail.com>
2021-12-06 23:44:50 +0530

    Clustering added

--- bigDataClassification.py
+++ bigDataClassification.py
@@ -10,6 +10,10 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml import Pipeline
 from pyspark.sql.functions import udf
 from pyspark.sql.types import DoubleType
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+pca = PCA(2)
 
 import os
 import sys
@@ -26,6 +30,9 @@ classifier3 = MultinomialNB()
 
 from sklearn.linear_model import PassiveAggressiveClassifier
 classifier5=PassiveAggressiveClassifier(random_state = 5)
+
+from sklearn.cluster import MiniBatchKMeans
+clustering = MiniBatchKMeans(n_clusters = 4)
 
 sc = SparkContext("local[2]","test")
 
@@ -77,23 +84,23 @@ def processBatch(input_rdd,process_id_cnt):
                 temp_row = []
                 for j in range(feat_len):
                     #attr_val will contain the value of the cell at row i and column j
-                	attr_val=str(json_obj_temp[i]['feature'+str(j)]).strip(' ')
-                	if(j==41):
+                    attr_val=str(json_obj_temp[i]['feature'+str(j)]).strip(' ')
+                    if(j==41):
                         #if the column is the target column("attack") then using the dictionary target_dict
                         #the main attack type is appended to the temp_row array
-                		temp_row.append(target_dict[attr_val])
-                	elif(j==1):
+                        temp_row.append(target_dict[attr_val])
+                    elif(j==1):
                         #if the column is the column("protocol_type") then using the dictionary protocol_dict
                         #the protocol type is mapped to an int val
-                		temp_row.append(protocol_dict[attr_val])
-                	elif(j==2 or j==3):
+                        temp_row.append(protocol_dict[attr_val])
+                    elif(j==2 or j==3):
                         #enter a dummy value later to be dropped
                         #unnecessary columns
-                		temp_row.append("tbr")
-                	else:
+                        temp_row.append("tbr")
+                    else:
                         #all other attr values are casted as float values
-                		temp_row.append(float(attr_val))
-                		
+                        temp_row.append(float(attr_val))
+                        
                 rdd_arr.append(tuple(temp_row))
             
             print("Recieved batch of data of length :",len(json_obj_temp.keys()))
@@ -121,28 +128,44 @@ def processBatch(input_rdd,process_id_cnt):
             
             labels = [] 
             if(file_currently_streaming == 'train'):
-            	print(X.shape,Y.shape)
+                print(X.shape,Y.shape)
                 #incremental training of the classifiers usin partial_fit 
-            	classifier2.partial_fit(X,Y.ravel(),classes=['DoS','Probe','R2L','U2R','Normal'])
-            	classifier3.partial_fit(X,Y.ravel(),classes=['DoS','Probe','R2L','U2R','Normal'])
-            	classifier5.partial_fit(X,Y.ravel(),classes=['DoS','Probe','R2L','U2R','Normal'])
-            	
-            	
+                classifier2.partial_fit(X,Y.ravel(),classes=['DoS','Probe','R2L','U2R','Normal'])
+                classifier3.partial_fit(X,Y.ravel(),classes=['DoS','Probe','R2L','U2R','Normal'])
+                classifier5.partial_fit(X,Y.ravel(),classes=['DoS','Probe','R2L','U2R','Normal'])
+                
+                
             else:
-            	print("its time to predict")
+                print("its time to predict")
 
-            	Y_test_preds = []
+                Y_test_preds = []
 
                 #predicting using the models trained before
-            	Y_test_preds=classifier2.predict(X)
-            	print("Test Accuracy-BernoulliNB      : {}".format(accuracy_score(Y, Y_test_preds)))
+                Y_test_preds=classifier2.predict(X)
+                print("Test Accuracy-BernoulliNB      : {}".format(accuracy_score(Y, Y_test_preds)))
 
-            	Y_test_preds=classifier3.predict(X.astype(np.float64))
-            	print("Test Accuracy-MultinomialNB      : {}".format(accuracy_score(Y, Y_test_preds)))
+                Y_test_preds=classifier3.predict(X.astype(np.float64))
+                print("Test Accuracy-MultinomialNB      : {}".format(accuracy_score(Y, Y_test_preds)))
 
-            	Y_test_preds=classifier5.predict(X.astype(np.float64))
-            	print("Test Accuracy-PassiveAggressiveClassifier      : {}".format(accuracy_score(Y, Y_test_preds)))            	
-            	            	
+                Y_test_preds=classifier5.predict(X.astype(np.float64))
+                print("Test Accuracy-PassiveAggressiveClassifier      : {}".format(accuracy_score(Y, Y_test_preds)))
+
+                print("The length for the labels is")
+                #fitting the test data clustering 
+                dataForClustering = pca.fit_transform(X)    
+                print("PCA data ",dataForClustering.shape)
+                labelsforBatch = clustering.fit_predict(dataForClustering)
+                print("labels are")
+                print(labelsforBatch)
+                #plotting the clusters in a scatter plot
+                for i in range(0,4):
+                    filteredFori = dataForClustering[labelsforBatch == i]
+                    print(filteredFori.shape)
+                    plt.scatter(filteredFori[:,0] , filteredFori[:,1],label = i) 
+                plt.legend()            
+                plt.show()
+                
+                                
 
             print("batch completed\n\n")
 
